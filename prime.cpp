65c18c420c6eafa7a61745072deb8a996f3364b5
Ashish Jadhav <114795586+ashish082003@users.noreply.github.com>
2022-12-29 22:33:30 +0530

    Create prime.cpp

---
+++ prime.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int n;
+cin>>n;
+bool prime=true;
+if(n==0 || n==1)
+{
+prime=false;
+}
+for(int i=0;i<n/2;i++){
+if(n%i==0){
+prime=false;
+}
+}
+if(prime){
+cout<<"n is prime number"<<endl;
+}
+else{
+cout<<"n is not prime number"<<endl;
+}
+return 0;
+}
